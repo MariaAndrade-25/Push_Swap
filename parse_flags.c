9b66c62eb5381fdd65c4453b9ba51a99ddf4e560
MariaAndrade-25 <mariadeandrade@hotmail.com.br>
2026-08-14 11:56:12 -0300

    Enhance parse_flag with validation and error handling
    Refactor parse_flag function to include validation checks for tokens and safe conversion of strings to integers.

--- parse_flags.c
+++ parse_flags.c
@@ -2,18 +2,38 @@
 #include <stdio.h>
 #include <limits.h>
 
-parse_flag(int argc, char **argv)
+int	parse_flag(int argc, char **argv)
 {
-    int i;
-    char **tokens;
+	int		i;
+	int		j;
+	int		value;
+	char	**tokens;
 
-    if (!argv)
-        return (0);
-    i = 1;
-    while (argv[i])
-    {
-        tokens = ft_split(argv[i], " ")
-        i++;
-    }
-    
+	if (!argv)
+		return (0);
+
+	i = 1;
+	while (argv[i])
+	{
+		tokens = ft_split(argv[i], ' ');
+		if (!tokens)
+			return (0);
+
+		j = 0;
+		while (tokens[j])
+		{
+			if (is_valid_number(tokens[j]) == 0)
+				return (0);
+
+			if (ft_atoi_safe(tokens[j], &value) == 0)
+				return (0);
+
+			j++;
+		}
+
+		// aqui depois você vai liberar tokens
+
+		i++;
+	}
+	return (1);
 }
